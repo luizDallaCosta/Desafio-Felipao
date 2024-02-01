d799d2f586385300e19d8d8c9692e882ab0cc6b8
LuizDallaCosta <luizdallacosta.pt@gmail.com>
2024-02-01 22:26:29 +0000

    primeiro commit

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
 let heroi = "Calabreso"
-let XP = 999
-
+let XP = 1000000
 switch (true) {
-    case XP <= 1000:
-      console.log("O Herói " + heroi + " está no nível de" + " Ferro");
-      break;
-   
-    default:
-      console.log("Final de semana! Descanse e divirta-se!");
-  }
+  case (XP <= 1000):
+    console.log("O Herói " + heroi + " está no nível de" + " Ferro");
+    break;
+
+  case (XP >= 1001) && (XP <2000):
+    console.log("O Herói " + heroi + " está no nível de" + " Bronze");
+    break;
+
+  case (XP >= 2001) && (XP <5000):
+    console.log("O Herói " + heroi + " está no nível de" + " Prata");
+    break;
+
+  case (XP >= 5001) && (XP <7000):
+    console.log("O Herói " + heroi + " está no nível de" + " Ouro");
+    break;
+
+  case (XP >= 7001) && (XP <8000):
+    console.log("O Herói " + heroi + " está no nível de" + " Platina");
+    break;
+
+  case (XP >= 8001) && (XP <9000):
+    console.log("O Herói " + heroi + " está no nível de" + " Ascendente");
+    break;
+
+  case (XP >= 9001) && (XP <10000):
+    console.log("O Herói " + heroi + " está no nível de" + " Imortal");
+    break;
+
+  case XP >= 10001:
+    console.log("O Herói " + heroi + " está no nível de" + " Radiante");
+    break;
+
+}
