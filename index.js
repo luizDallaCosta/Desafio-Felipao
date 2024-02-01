03190cc6c31af52cb96a7dbf680250195af6f504
LuizDallaCosta <luizdallacosta.pt@gmail.com>
2024-02-01 06:29:14 +0000

    primeiro commit

---
+++ index.js
@@ -0,0 +1,11 @@
+let heroi = "Calabreso"
+let XP = 999
+
+switch (true) {
+    case XP <= 1000:
+      console.log("O Herói " + heroi + " está no nível de" + " Ferro");
+      break;
+   
+    default:
+      console.log("Final de semana! Descanse e divirta-se!");
+  }
